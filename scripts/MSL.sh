6dfb1294fa45affeec48d49efbb4f772e53b4d7a
Khuu Trong Quan <57951538+khuutrongquan@users.noreply.github.com>
2025-12-07 07:57:10 +0700

    Update MSL.sh

--- scripts/MSL.sh
+++ scripts/MSL.sh
@@ -1,5 +1,5 @@
---anormly_ratio 0.705 --num_epochs 5   --batch_size 64  --mode train --dataset MSL  --data_path dataset/MSL --input_c 55    --output_c 55
---anormly_ratio 0.705  --num_epochs 5      --batch_size 64     --mode test    --dataset MSL   --data_path dataset/MSL  --input_c 55    --output_c 55
+--anormly_ratio 0.629 --num_epochs 5   --batch_size 64  --mode train --dataset MSL  --data_path dataset/MSL --input_c 55    --output_c 55
+--anormly_ratio 0.629  --num_epochs 5      --batch_size 64     --mode test    --dataset MSL   --data_path dataset/MSL  --input_c 55    --output_c 55
 
 
 
